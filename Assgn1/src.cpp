040671b9094a38982e6cb490d974eec0a01e2b06
atharva-sarage <atharva.sarage@gmail.com>
2021-02-01 00:35:17 +0530

    Vector time output format done

--- Assgn1/src.cpp
+++ Assgn1/src.cpp
@@ -11,13 +11,12 @@ using namespace std;
 map <pair<int,int>,int> clientPortMap,clientServerSocket;
 map <pair<int,int>,int> port_idx;
 map <pair<int,int>,int> sockfdMap;
+std::default_random_engine eng;
 ofstream output; 
 int serverPortSeed,clientPortSeed,m,l1,alpha,n;
 set <int> WaitingSet;
-int random(int a, int b) {    
-    int out = a + rand() % (b - a + 1);
-    return out;
-}
+set <pair<pair<int,int>,int>> pendingMessageSet;
+
 /**
  * Helper Class for get the formatted time in HH:MM:SS 
  * */
@@ -30,7 +29,17 @@ class Helper {
         sprintf(buffer,"%d : %d : %d",t2->tm_hour,t2->tm_min,t2->tm_sec);
         return buffer;
     }
+    static int getRandomNumber(int a, int b) 
+    {    
+        int out = a + rand() % (b - a + 1);
+        return out;
+    }
 };
+
+// make a new class to replace the global variables
+// class GlobaNodeManager{
+
+// }
 class Node{
 
     int id;
@@ -120,21 +129,27 @@ class Node{
 
             while( recvLen =  recv(socketToListen, buffer, BUFSIZE - 1, 0) > 0){
                 string message = string(buffer);
-                //cout<<message<<" %%%% "<<socketToListen<<endl;
-                vector <int> sendersVector = parseString(message);
-               
-                timeVectorLock.lock();
-                for(int i=0;i<n;i++){
-                    timeVector[i] = max(timeVector[i] , sendersVector[i]);
-                }
-                time_t RecvTime=time(NULL);
-            	string formatted_time=Helper::get_formatted_time(RecvTime);
-                string FinalString = "Process "+to_string(id) + "receives message m"+to_string(clientId)+to_string(++(recvFreq[clientId])) +"from process"+to_string(clientId)+ "at "+ formatted_time +" , vc: "+ outputString()+" incomming string "+ message +"\n";
-                output<<FinalString;
-                cout<<"Recieved Message Server id::" <<id<<" vc: "<<outputString()<<endl;                
+                cout<<message<<" %%%% "<<socketToListen<<endl;
+                vector <vector <int> > sendersVector = parseString(message);
+                for(auto senderVector : sendersVector){
+                    for(auto k1:senderVector){
+                        cout<<k1<<" * ";
+                    }
+                    cout<<"###"<<endl;
+                    timeVectorLock.lock();
+                    for(int i=0;i<n;i++){
+                        timeVector[i] = max(timeVector[i] , senderVector[i]);
+                    }
+                    time_t RecvTime=time(NULL);
+                    string formatted_time=Helper::get_formatted_time(RecvTime);
+                    string FinalString = "process"+to_string(id) + " receives message m"+to_string(clientId)+to_string(++(recvFreq[clientId])) +" from process"+to_string(clientId)+ " at "+ formatted_time +" , vc: "+ outputTimeVectorString()+" incomming string "+ message +"\n";
+                    output<<FinalString;
+                    pendingMessageSet.erase({{id,clientId},recvFreq[clientId]});
+                    cout<<"Recieved Message Server id::" <<id<<" vc: "<<outputTimeVectorString()<<endl;                
 
-                timeVectorLock.unlock();
-                memset(buffer, 0, BUFSIZE);
+                    timeVectorLock.unlock();
+                    memset(buffer, 0, BUFSIZE);
+                }
             }
 
         }
@@ -188,34 +203,36 @@ class Node{
         }
 
         void sendMessage(){
+            std::exponential_distribution<double>exponential(1.0/l1); // to generate sleep times having exponential distribution
             for(int i=1;i<=m;i++){
-                int randomNumber = rand()%5;
+                int randomNumber = Helper::getRandomNumber(1,5);
                 if(randomNumber < 3){ // internal event
                     timeVectorLock.lock();
                     timeVector[id-1] =  timeVector[id-1] + 1 ;                  
                     timeVectorLock.unlock();
-                    cout<<"Internal Event "<< id<<" "<<outputString()<<endl;
+                    cout<<"Internal Event "<< id<<" "<<outputTimeVectorString()<<endl;
                     string message = compressTimeVector();
                     time_t InternalEventTime=time(NULL);
             		string formatted_time=Helper::get_formatted_time(InternalEventTime);                    
-                    string FinalString = "Process "+to_string(id) + "executes internal event e"+to_string(id)+to_string(internalFreq++) +" at "+ formatted_time +" , vc: "+ message+"\n";
+                    string FinalString = "process"+to_string(id) + " executes internal event e"+to_string(id)+to_string(++internalFreq) +" at "+ formatted_time +" , vc: "+ message+"\n";
                     output<<FinalString;
                 }else{ // message send
-                    int reciever = outDegreeVertices[rand()%outDeg] ;
+                    int randomOutDegreeIndex = Helper::getRandomNumber(0,outDeg-1);
+                    int reciever = outDegreeVertices[randomOutDegreeIndex] ;
                     int recieverSocket = clientServerSocket[{reciever,id}];
-                    string message = compressTimeVector();
+                    string message = outputTimeVectorString();
                     ssize_t sentLen = send(recieverSocket,message.c_str(), strlen(message.c_str()), 0);
                     // log event to file
-                    //Process3 sends message m31 to process2 at 10:02, vc: [0 0 1 0]
+                    //process3 sends message m31 to process2 at 10:02, vc: [0 0 1 0]
                     time_t SendTime=time(NULL);
             		string formatted_time=Helper::get_formatted_time(SendTime);
                     
-                    string FinalString = "Process "+to_string(id) + "sends message m"+to_string(id)+to_string(sendFreq[reciever]++) +"to Process"+to_string(reciever)+ "at "+ formatted_time +" , vc: "+ message+"\n";
+                    string FinalString = "process"+to_string(id) + " sends message m"+to_string(id)+to_string(++sendFreq[reciever]) +" to process"+to_string(reciever)+ " at "+ formatted_time +" , vc: "+ message+"\n";
                     output<<FinalString;
-
+                    pendingMessageSet.insert({{reciever,id},sendFreq[reciever]});
                     cout<<id<<" "<<" Sending message to "<< reciever<<" "<<message<<"--- "<<recieverSocket<<endl;
-                }
-                sleep(3);
+                }           
+                usleep(exponential(eng)*1000);
             }
 		}
         string compressTimeVector(){
@@ -226,19 +243,28 @@ class Node{
             }
             return message;
         }
-        vector<int> parseString(string str){
-            vector<int> senderTimeVector;
+        vector< vector<int> > parseString(string str){
+            vector< vector<int> > senderTimeVector;
             for(int i=0;i<str.size();i++){
-                string temp;
-                while(str[i] != '*'){
-                    temp+=str[i];
+                if(str[i]=='['){
                     i++;
-                }
-                senderTimeVector.push_back(stoi(temp));
+                    string temp;
+                    vector <int> tempVec;
+                    while(str[i] != ']'){
+                        while(str[i] != ' ' && str[i]!= ']'){
+                            temp+=str[i];
+                            i++;
+                        }
+                        if(str[i]==' ') i++;
+                        tempVec.push_back(stoi(temp));
+                        temp.clear();
+                    }
+                    senderTimeVector.push_back(tempVec);
+                }               
             }
             return senderTimeVector;
         }
-        string outputString(){
+        string outputTimeVectorString(){
             string message;
             message += '[';
             for(auto k:timeVector){
@@ -336,8 +362,8 @@ int main()
     }
 
     srand(time(NULL));
-    serverPortSeed = random(10000,11000);
-    clientPortSeed = random(11000 ,12000);
+    serverPortSeed = Helper::getRandomNumber(10000,11000);
+    clientPortSeed = Helper::getRandomNumber(11000 ,12000);
 
     for(int i=1;i<=n;i++){      
         nodes[i] = new Node(inverseAdjacencyList[i],adjacencyList[i],i);
@@ -355,6 +381,6 @@ int main()
     for(int i=1;i<=n;i++){
         nodes[i]->sendMessageThreads();
     }
-    // don't terminate untill all the messages are done
-    //sleep(100) ;
+    while(!pendingMessageSet.empty());
+    
 }
--- Assgn1/test.cpp
+++ Assgn1/test.cpp
@@ -6,9 +6,35 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 using namespace std;
+vector< vector<int> > parseString(string str){
+    vector< vector<int> > senderTimeVector;
+    for(int i=0;i<str.size();i++){
+        if(str[i]=='['){
+            i++;
+            string temp;
+            vector <int> tempVec;
+            while(str[i] != ']'){
+                while(str[i] != ' ' && str[i]!= ']'){
+                    temp+=str[i];
+                    i++;
+                }
+                if(str[i]==' ') i++;
+                tempVec.push_back(stoi(temp));
+                temp.clear();
+            }
+            senderTimeVector.push_back(tempVec);
+        }               
+    }
+    return senderTimeVector;
+}
 int main(){
     int a = 10;
     int b =htons(a);
     int c = ntohs(b);
-    cout<<a<<" "<<b<<" "<<c<<endl;
+    vector< vector<int> > ans = parseString("[10 0 10][11 4 5][50 6 7000]");
+    for(auto k:ans){
+        for(auto l:k)
+            cout<<l<<" ";
+        cout<<endl;
+    }
 }

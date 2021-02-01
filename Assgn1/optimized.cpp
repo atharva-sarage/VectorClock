0f7bfc8cbf44e2a9ba3d670cf4add5c48cc69d98
atharva-sarage <atharva.sarage@gmail.com>
2021-02-01 11:54:58 +0530

    Optimized version done

--- Assgn1/optimized.cpp
+++ Assgn1/optimized.cpp
@@ -59,6 +59,9 @@ class Node{
     int* sendFreq;
     int* recvFreq;
     int internalFreq = 0;
+    int* lastSent;
+    int* lastUpdate;
+    mutex setLock;
 
     private:
         void initServerNode(){
@@ -129,25 +132,34 @@ class Node{
 
             while( recvLen =  recv(socketToListen, buffer, BUFSIZE - 1, 0) > 0){
                 string message = string(buffer);
-                cout<<message<<" %%%% "<<socketToListen<<endl;
-                vector <vector <int> > sendersVector = parseString(message);
+                vector <vector <pair<int,int>> > sendersVector = parseString(message);
                 for(auto senderVector : sendersVector){
-                    for(auto k1:senderVector){
-                        cout<<k1<<" * ";
-                    }
-                    cout<<"###"<<endl;
                     timeVectorLock.lock();
-                    for(int i=0;i<n;i++){
-                        timeVector[i] = max(timeVector[i] , senderVector[i]);
+                    timeVector[id-1] =  timeVector[id-1] + 1 ;    
+                    lastUpdate[id] = timeVector[id-1];
+                   
+                    vector<int>updatedEntries;
+                    for(auto k1:senderVector){
+                        if(k1.second > timeVector[k1.first - 1]){
+                            updatedEntries.push_back(k1.first);
+                            timeVector[k1.first -1 ] = k1.second;
+                        }
                     }
+                    for(auto entry:updatedEntries){
+                        lastUpdate[entry] = timeVector[id-1];
+                    }
+                    timeVectorLock.unlock();
+
                     time_t RecvTime=time(NULL);
                     string formatted_time=Helper::get_formatted_time(RecvTime);
-                    string FinalString = "process"+to_string(id) + " receives message m"+to_string(clientId)+to_string(++(recvFreq[clientId])) +" from process"+to_string(clientId)+ " at "+ formatted_time +" , vc: "+ outputTimeVectorString()+" incomming string "+ message +"\n";
+                    string FinalString = "process"+to_string(id) + " receives message m"+to_string(clientId)+to_string(++(recvFreq[clientId])) +" from process"+to_string(clientId)+ " at "+ formatted_time +" , vc: "+ outputTimeVectorString()+"\n"; //+" incomming string "+ message +"\n";
                     output<<FinalString;
+                   
+                    setLock.lock();
                     pendingMessageSet.erase({{id,clientId},recvFreq[clientId]});
+                    cout<<"<"<<id<<" "<<clientId<<" "<<recvFreq[clientId]<<endl;
+                    setLock.unlock();
                     cout<<"Recieved Message Server id::" <<id<<" vc: "<<outputTimeVectorString()<<endl;                
-
-                    timeVectorLock.unlock();
                     memset(buffer, 0, BUFSIZE);
                 }
             }
@@ -205,36 +217,47 @@ class Node{
         void sendMessage(){
             std::exponential_distribution<double>exponential(1.0/l1); // to generate sleep times having exponential distribution
             for(int i=1;i<=m;i++){
-                timeVectorLock.lock();
-                timeVector[id-1] =  timeVector[id-1] + 1 ;                  
-                timeVectorLock.unlock();
                 int randomNumber = Helper::getRandomNumber(1,5);
-                if(randomNumber < 3){ // internal event
-                   
+                timeVectorLock.lock();
+                timeVector[id-1] =  timeVector[id-1] + 1 ;    
+                lastUpdate[id] = timeVector[id-1];
+                timeVectorLock.unlock();
+                if(randomNumber < 3){ // internal event                  
                     cout<<"Internal Event "<< id<<" "<<outputTimeVectorString()<<endl;
-                    string message = compressTimeVector();
                     time_t InternalEventTime=time(NULL);
             		string formatted_time=Helper::get_formatted_time(InternalEventTime);                    
-                    string FinalString = "process"+to_string(id) + " executes internal event e"+to_string(id)+to_string(++internalFreq) +" at "+ formatted_time +" , vc: "+ message+"\n";
+                    string FinalString = "process"+to_string(id) + " executes internal event e"+to_string(id)+to_string(++internalFreq) +" at "+ formatted_time +" , vc: "+ outputTimeVectorString()+"\n";
                     output<<FinalString;
                 }else{ // message send
                     int randomOutDegreeIndex = Helper::getRandomNumber(0,outDeg-1);
                     int reciever = outDegreeVertices[randomOutDegreeIndex] ;
                     int recieverSocket = clientServerSocket[{reciever,id}];
-                    string message = outputTimeVectorString();
+                    string message = getUpdatedMessagePairs(reciever);                   
+                    cout<<message<<endl;
                     ssize_t sentLen = send(recieverSocket,message.c_str(), strlen(message.c_str()), 0);
                     // log event to file
                     //process3 sends message m31 to process2 at 10:02, vc: [0 0 1 0]
                     time_t SendTime=time(NULL);
             		string formatted_time=Helper::get_formatted_time(SendTime);
                     
-                    string FinalString = "process"+to_string(id) + " sends message m"+to_string(id)+to_string(++sendFreq[reciever]) +" to process"+to_string(reciever)+ " at "+ formatted_time +" , vc: "+ message+"\n";
+                    string FinalString = "process"+to_string(id) + " sends message m"+to_string(id)+to_string(++sendFreq[reciever]) +" to process"+to_string(reciever)+ " at "+ formatted_time +" , vc: "+ outputTimeVectorString()+"\n";
                     output<<FinalString;
-                    pendingMessageSet.insert({{reciever,id},sendFreq[reciever]});
-                    cout<<id<<" "<<" Sending message to "<< reciever<<" "<<message<<"--- "<<recieverSocket<<endl;
+                    setLock.lock();
+                    if(pendingMessageSet.find({{reciever,id},sendFreq[reciever]}) != pendingMessageSet.end())
+                        pendingMessageSet.insert({{reciever,id},sendFreq[reciever]});
+                    else
+                        pendingMessageSet.erase({{reciever,id},sendFreq[reciever]});
+                    setLock.unlock();
+                    cout<<">"<<reciever<<" "<<id<<" "<<sendFreq[reciever]<<endl;
+                    cout<<id<<" "<<" Sending message to "<< reciever<<" "<<message<<"--- "<<recieverSocket<<" "<<timeVector[id-1]<<endl;                    
+                    setLock.unlock();
+                    timeVectorLock.lock();
+                    lastSent[reciever] = timeVector[id-1];
+                    timeVectorLock.unlock();
                 }           
                 usleep(exponential(eng)*1000);
             }
+            cout<<"Server "<<id<<" done\n";
 		}
         string compressTimeVector(){
             string message;
@@ -244,21 +267,45 @@ class Node{
             }
             return message;
         }
-        vector< vector<int> > parseString(string str){
-            vector< vector<int> > senderTimeVector;
+        string getUpdatedMessagePairs(int clientId){
+            string message = "[";
+
+            for(int i=1;i<=n;i++){
+                cout<<lastSent[clientId]<<" "<<lastUpdate[i]<<endl;
+                if(lastSent[clientId] < lastUpdate[i]){
+                    message +='(';
+                    message += to_string(i);
+                    message += ',';
+                    message += to_string(timeVector[i-1]);
+                    message += ')';
+                }
+            }
+            message+=']';
+            return message;
+        }
+        vector< vector<pair<int,int>> > parseString(string str){
+            vector< vector<pair<int,int>> > senderTimeVector;
             for(int i=0;i<str.size();i++){
                 if(str[i]=='['){
                     i++;
-                    string temp;
-                    vector <int> tempVec;
+                    string temp,temp2;
+                    vector <pair<int,int>> tempVec;
                     while(str[i] != ']'){
-                        while(str[i] != ' ' && str[i]!= ']'){
+                        if(str[i] == '(') i++;
+                        while(str[i] != ','){
                             temp+=str[i];
                             i++;
                         }
-                        if(str[i]==' ') i++;
-                        tempVec.push_back(stoi(temp));
+
+                        if(str[i] == ',')i++;
+                        while(str[i] != ')'){
+                            temp2+=str[i];
+                            i++;
+                        }
+                        if(str[i]==')')i++;
+                        tempVec.push_back({stoi(temp),stoi(temp2)});
                         temp.clear();
+                        temp2.clear();
                     }
                     senderTimeVector.push_back(tempVec);
                 }               
@@ -277,6 +324,8 @@ class Node{
 
             return message;
         }
+              
+
         void initClientListnerThreads(){
             for(int i=0;i<inDeg;i++)
                 {
@@ -311,6 +360,8 @@ class Node{
         memset(recvFreq,0,sizeof(recvFreq));
         this->id = id;  
         timeVector.resize(n);   	        
+        lastSent = new int[n+1];
+        lastUpdate = new int[n+1];
         init();
     }
     void startListenerThreads(){
@@ -382,6 +433,9 @@ int main()
     for(int i=1;i<=n;i++){
         nodes[i]->sendMessageThreads();
     }
-    while(!pendingMessageSet.empty());
+    while(!pendingMessageSet.empty()){
+        cout<<pendingMessageSet.size()<<" ---"<<(*pendingMessageSet.begin()).first.first<<" "<<(*pendingMessageSet.begin()).second<<" "<<(*pendingMessageSet.begin()).second<<endl;
+        sleep(2);
+    }
     
 }
--- Assgn1/test.cpp
+++ Assgn1/test.cpp
@@ -27,14 +27,43 @@ vector< vector<int> > parseString(string str){
     }
     return senderTimeVector;
 }
+  vector< vector<pair<int,int>> > parseString2(string str){
+    vector< vector<pair<int,int>> > senderTimeVector;
+    for(int i=0;i<str.size();i++){
+        if(str[i]=='['){
+            i++;
+            string temp,temp2;
+            vector <pair<int,int>> tempVec;
+            while(str[i] != ']'){
+                if(str[i] == '(') i++;
+                while(str[i] != ','){
+                    temp+=str[i];
+                    i++;
+                }
+
+                if(str[i] == ',')i++;
+                while(str[i] != ')'){
+                    temp2+=str[i];
+                    i++;
+                }
+                if(str[i]==')')i++;
+                tempVec.push_back({stoi(temp),stoi(temp2)});
+                temp.clear();
+                temp2.clear();
+            }
+            senderTimeVector.push_back(tempVec);
+        }               
+    }
+    return senderTimeVector;
+}
 int main(){
     int a = 10;
     int b =htons(a);
     int c = ntohs(b);
-    vector< vector<int> > ans = parseString("[10 0 10][11 4 5][50 6 7000]");
+    vector< vector<pair<int,int>> > ans = parseString2("[(10,20)(330,40)][(1,5)(7,8)]");
     for(auto k:ans){
         for(auto l:k)
-            cout<<l<<" ";
+            cout<<l.first<<" "<<l.second<<" * ";
         cout<<endl;
     }
 }
